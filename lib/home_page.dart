0b98b43284c7113e3db66666f760c7adf495f6f5
sayed <alsydkrswm0@gmail.com>
2026-08-08 17:58:22 +0300

    Build hide app bar by two ways 1- NestedScrollView  2- CustomScrollViw

---
+++ lib/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.widget});
+  final Widget widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return widget;
+  }
+}
